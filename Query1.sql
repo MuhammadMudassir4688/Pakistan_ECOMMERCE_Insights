41736a2105630e4f3b5e58cacfe3944d470de9c0
MuhammadMudassir4688 <63054406+MuhammadMudassir4688@users.noreply.github.com>
2024-08-11 16:13:05 +0500

    Ecommerce Insight - Initial Commit
    Here you can find dataset, powerbi file, pptx,SQL and Python notebook used in this project

---
+++ Query1.sql
@@ -0,0 +1,10 @@
+select top 10 * from Ecommerce_data
+--ALTER TABLE Ecommerce_data
+--DROP COLUMN column26;
+--column22 to 26 dropped
+
+
+--DELETE FROM Ecommerce_data WHERE item_id IS null
+
+select count(distinct customer_id) from Ecommerce_data
+--115327
